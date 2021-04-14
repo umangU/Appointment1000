9de739ba24c49d150195562ed74a790d3eecab9b
umangU <72771903+umangU@users.noreply.github.com>
2021-04-15 08:20:26 +1000

    Code Updated

--- QDA.R
+++ QDA.R
@@ -1,25 +1,25 @@
-#Loading the Mass and caTools for libraries for qda() and sample.split() function respectively
+# Loading the Mass and caTools for libraries for qda() and sample.split() function respectively
 library(MASS)
 library(caTools)
-#Reading the appointment1000.csv in R
+# Reading the appointment1000.csv in R
 appoint=read_xlsx(file.choose())
 attach(appoint)
-#Fitting the original dataset in the Logistic Regression model
+# Fitting the original dataset in the Logistic Regression model
 qda.fit=qda(No.show~Gender+Age+Hypertension+Diabetes+Alcoholism+Disability+SMS_received, data=appoint)
 qda.fit
-#Set Seed
+# Set Seed
 set.seed(123, kind = "Mersenne-Twister", normal.kind = "Inversion")
-#Divide the dataset based on split ratio
+# Divide the dataset based on split ratio
 sample = sample.split(appoint, SplitRatio = .6)
-#Segregate the train and test data based on the above split ratio
+# Segregate the train and test data based on the above split ratio
 train = subset(appoint, sample == TRUE)
 test = subset(appoint, sample == FALSE)
-#Using Predict to get a list of three elements namely 'class', 'posterior' and 'x'
+# Using Predict to get a list of three elements namely 'class', 'posterior' and 'x'
 qda.class=predict(qda.fit,test)$class 
-#'class' contains the predictions about the people going as per their appointments or not
-#Confusion matrix between the predicted values and the original values in the test dataset
+# 'class' contains the predictions about the people going as per their appointments or not
+# Confusion matrix between the predicted values and the original values in the test dataset
 table(qda.class,test$No.show)
-#calculating the mean to find out the proportion of correct predictions
+# calculating the mean to find out the proportion of correct predictions
 mean(qda.class==test$No.show)
-#calculating the mean to find out the proportion of incorrect predictions (test error rate)
+# calculating the mean to find out the proportion of incorrect predictions (test error rate)
 mean(qda.class!=test$No.show)
